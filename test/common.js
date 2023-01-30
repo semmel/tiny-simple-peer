09f8a6399c06789ab3826ebc00d0ea30af8e4f7c
Diego Rodríguez Baquero <github@diegorbaquero.com>
2023-01-30 18:06:01 -0500

    Update common.js

--- test/common.js
+++ test/common.js
@@ -20,7 +20,7 @@ exports.getConfig = thunky(function (cb) {
 })
 
 // For testing on node, we must provide a WebRTC implementation
-if (process.env.WRTC?.startsWith('wrtc')) {
+if (process.env.WRTC === 'wrtc') {
   exports.wrtc = require('wrtc')
 }
 
